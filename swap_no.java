ba90c9c72106f66435f1e97fca35fc796c8085f8
Prabhat Yadav <67184304+pra-y@users.noreply.github.com>
2021-10-04 00:06:11 +0530

    Update swap_no.java

--- swap_no.java
+++ swap_no.java
@@ -1,12 +1,10 @@
-import java.util.*;  
 class Swap   
 {  
     public static void main(String a[])   
     {   
         System.out.println("Enter the value of x and y");  
         
-	Scanner sc = new Scanner(System.in);  
-        int x = 223;
+	int x = 223;
         int y = 837;  
         System.out.println("Before swapping numbers: "+x +" "+ y);  
        
